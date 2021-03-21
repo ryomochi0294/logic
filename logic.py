f65572b2d9c8bb23eb1400fbd4178b744ff12562
malcolm-weathers <mwea@protonmail.com>
2021-03-21 18:36:00 -0400

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,57 +1,73 @@
 import itertools
 
-def main2():
-    all_vars = ['cheetah','gazelle','savanna']
-    know = ['eats,cheetah,gazelle','livesin,gazelle,savanna',
-                 'livesin,cheetah,savanna']
-    rules = ['predator,X=eats,X,any.Y','eatsall,X=eats,X,all.y']
+
+all_vars = ['cheetah','gazelle','savanna']
+know = ['eats,cheetah,gazelle','livesin,gazelle,savanna',
+                 'livesin,cheetah,savanna']    
+rules = ['predator(X)=eats,any.X,any.Y',
+             'eatsall,X=eats,any.X,all.y']
     
-    #query = 'predator,cheetah'
-    #query2 = 'predator,gazelle'
-    #print('Cheetah is predator: {}'.format(check2(all_vars,know,rules,query)))
-    #print('Gazelle is predator: {}'.format(check2(all_vars,know,rules,query2))
-    #print(eval2(know, 'eats,cheetah,gazelle'))
-    #print(eval2(know, 'eats,gazelle,cheetah'))
-    #print(query_gen(all_vars, know, rules[0]))
-    print(generate(all_vars, know, rules[0]))
+    #qs = gen(all_vars,know,rules[0])
+
+"""def gen(all_vars,know,rule):
+    fv,r = rule.split('=')
+    sp = r.split(',')
+    #print(sp)
+    k = sp[0]
+    combos = itertools.combinations_with_replacement(all_vars, len(sp)-1)
+    c = list(combos)
+    #print(c)
+
+    x = [','.join([k]+list(y)) for y in c]
+    y = [z in know for z in x]
+
+    fv,r = rule.split('=')
+    f,v = fv.split(',')
+
+    for k in know:
+        exec('def {}({}):\n\treturn ')"""
 
 
-#def check2(know, rules, query):
-    # define functions
-
-
-def generate(all_vars, know, rule):
-    prov, cond = rule.split('=')
-    func, var = prov.split(',', 1)
-
-    conditional_function, rest = cond.split(',', 1)
-    first_var, rest = rest.split(',', 1)
+# istrue(['any.X','any.Y'],0,2,'eats')
+def convrule(rule):
+    fv, rest = rule.split('=')
     rest = rest.split(',')
+    func = rest.pop(0)
+    ll = len(rest)
+    print(rest,0,ll,func)
+    print(any(istrue(rest,0,ll,func)))
 
-    all_queries = []
-    # Checking X
-    for var in all_vars:
-        query = conditional_function + ',' + var
-        combinations = itertools.combinations(all_vars, len(rest))
-        for combination in combinations:
-            query_test = ','.join([query]+list(combination))
-            all_queries.append(query_test)
-    return all_queries
+# vtype: ['any.X','any.Y']
+# vvalue: ['cheetah','gazelle']
+# index: 1
+# maxv: 2
+# cq = 'eats,'
 
-def construct(know, rules):
-    while True:
-        did_change = False
-
-        for rule in rules:
-            results = generate(know, rule)
-            if results != []:
-                did_change = True
-
-        if not did_change:
-            return False
-
-def eval2(know, stmt):
-    return stmt in know
+def istrue(vtype, index, maxv, cq):
+    if index < maxv:
+        if vtype[index].startswith('any.'):
+            res = []
+            for v in all_vars:
+                res.append(istrue(vtype,index+1,maxv,cq+','+v))
+            if any(res):
+                return res
+            else:
+                return []
+        elif vtype[index].startswith('all.'):
+            res = []
+            for v in all_vars:
+                res.append(istrue(vtype,index+1,maxv,cq+','+v))
+            if all(res):
+                return res
+            else:
+                return []
+    elif index == maxv:
+        if cq in know:
+            return cq
+        else:
+            return []
 
 if __name__ == '__main__':
-    main2()
+    #print(istrue(['any.X','any.Y'],0,2,'eats'))
+    convrule(rules[0])
+    convrule(rules[1])
