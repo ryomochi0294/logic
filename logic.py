7212930c01e605b93724c87d190548c0edcd49db
malcolm-weathers <mwea@protonmail.com>
2021-03-21 14:45:20 -0400

    Create logic.py
    Basic structure

---
+++ logic.py
@@ -0,0 +1,35 @@
+
+
+def check_rule(all_vars, knowledge_base, rule):
+    for var in all_vars:
+        exec('%s = %s' % (var, var in knowledge_base))
+    cond, prov = rule.split('->')
+    return eval(cond)
+
+def main():
+    all_vars = set(['T','B','cat','F'])
+    know = set('T')
+    rules = set(['T or B->cat','cat->F'])
+    checked = set()
+
+    while True:
+        did_change = False
+        for rule in rules:
+            result = check_rule(all_vars, know, rule)
+            if result == True:
+                cond, prov = rule.split('->')
+                if prov == 'F':
+                    return True
+                know.add(prov)
+                checked.add(rule)
+                did_change = True
+
+        if not did_change:
+            return False
+
+        for rule in checked:
+            if rule in rules:
+                rules.remove(rule)
+
+if __name__ == '__main__':
+    print(main())
