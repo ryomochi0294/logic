405ebbb9e478f5a1276b5254459334c16534142d
ryomochi0294 <32785065+ryomochi0294@users.noreply.github.com>
2021-03-23 18:31:32 -0400

    Update logic.py
    Still not complete

--- logic.py
+++ logic.py
@@ -1,33 +1,38 @@
 import itertools, re
 
 
-all_vars = ['cheetah','gazelle','savanna']
+all_vars = ['cheetah','gazelle','chicken','grass','savanna']
 know = ['eats,cheetah,gazelle',
-        'eats,cheetah,gazelle',
-        'eats,cheetah,gazelle',
+        'eats,cheetah,chicken',
+        'eats,gazelle,grass',
         'livesin,gazelle,savanna',
         'livesin,cheetah,savanna']    
 rules = ['predator(X)=eats,any.X,any.Y',
-             'eatsall,X=eats,any.X,any.y,any.Z']
+             'TopOfChain(X)=eats,any.X,all.Y',
+              'predator(X)=eats,any.X,any.Y and location(Y)=livesin,any.Y,any.Z and predator(Y)=eats,any.Y,any.Z']
 
 
 # istrue(['any.X','any.Y'],0,2,'eats')
 # Convert a string containing a rule into the format used by istrue()
-def convrule(rule):
+def convrule(rule,round):
     fv, rest = rule.split('=')
     rest = rest.split(',')
     func = rest.pop(0)
     ll = len(rest)
-    print(rest,0,ll,func)
+    if(round == 0):
+        print(rest,0,ll,func)
     x = istrue(rest,0,ll,func)
-
     for result in x:
         print(result)
         func, var_being_updated = fv.split('(')
         var_being_updated = var_being_updated.replace(')','')
         location_in_query = rest.index('any.'+var_being_updated)
-        know = '{},{}'.format(func,result.split(',')[location_in_query+1])
-        print(know)
+        if(func == 'predator' or func == 'TopOfChain'):
+            check = result.split(',')[location_in_query+1]
+        elif(func == 'location'):
+            check = result.split(',')[location_in_query+2]
+        know = '{},{}'.format(func,check)
+        return know
     
     #print(x)
     #print(any(x))
@@ -35,10 +40,15 @@ def convrule(rule):
 def compound(rule):
     r = re.split(' and | or ', rule)
     print(r)
+    mem= []
+    count = 0
     for rp in r:
-        rparts = convrule(rp)
-        print(rparts)
-    
+        rparts = convrule(rp,count)
+        if rparts not in mem:
+            mem.append(rparts)
+        count = count + 1
+    print('\n')
+    print(*mem)
     return
 
 # vtype: ['any.X','any.Y']
@@ -93,5 +103,6 @@ def istrue(vtype, index, maxv, cq):
 
 if __name__ == '__main__':
     #print(istrue(['any.X','any.Y'],0,2,'eats'))
-    convrule(rules[0])
+    compound(rules[2])
     #convrule(rules[1])
+
